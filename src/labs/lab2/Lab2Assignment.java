8201de6f657f2644db1cd987920685b915af5ba3
DarbyORear <darbyorear@gmail.com>
2018-07-17 11:59:00 -0400

    added a comment

--- src/labs/lab2/Lab2Assignment.java
+++ src/labs/lab2/Lab2Assignment.java
@@ -3,6 +3,7 @@ package labs.lab2;
 import java.util.Scanner;
 
 public class Lab2Assignment {
+	//Note to modify file
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
